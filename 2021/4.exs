0bfe25376f0c202fbbf9ef780d7dd760db332882
Saverio Trioni <saverio.trioni@gmail.com>
2021-12-05 02:05:59 +0000

    simplify day 4

--- 2021/4.exs
+++ 2021/4.exs
@@ -1,59 +1,46 @@
 #! /usr/bin/env elixir
 
-defmodule B do
-  import Enum
+import Enum
 
-  def wins?(b), do: any?(b, fn r -> empty?(r) end)
-  def score(b), do: b |> reduce(&MapSet.union/2) |> sum()
+[nums | boards] = "input/2021/4.txt" |> File.read!() |> String.split("\n\n", trim: true)
 
-  def run do
-    [nums | boards] = "input/2021/4.txt" |> File.read!() |> String.split("\n\n", trim: true)
+nums =
+  nums
+  |> String.split(",")
+  |> map(&String.to_integer/1)
 
-    nums =
-      nums
-      |> String.split(",")
-      |> map(&String.to_integer/1)
+boards =
+  map(boards, fn b ->
+    b = b |> String.split() |> map(&String.to_integer/1)
+    rows = chunk_every(b, 5)
+    cols = zip_with(rows, & &1)
+    map(cols ++ rows, &MapSet.new/1)
+  end)
 
-    boards =
-      map(boards, fn b ->
-        b = b |> String.split() |> map(&String.to_integer/1)
-        rows = chunk_every(b, 5)
-        cols = zip_with(rows, & &1)
-        map(cols ++ rows, &MapSet.new/1)
-      end)
+game =
+  {nil, nums, boards, []}
+  |> Stream.iterate(fn {_, [n | rest], boards, _} ->
+    {new_winners, still_playing} =
+      boards
+      |> map(fn board -> map(board, fn line -> MapSet.delete(line, n) end) end)
+      |> split_with(fn board -> any?(board, &empty?/1) end)
 
-    game =
-      Stream.iterate({nil, nums, boards}, fn {_, [x | rest], b} ->
-        new_b =
-          map(b, fn board ->
-            map(board, fn r -> MapSet.delete(r, x) end)
-          end)
+    {n, rest, still_playing, new_winners}
+  end)
+  |> Stream.reject(&match?({_, _, _, []}, &1))
 
-        {x, rest, new_b}
-      end)
+{nxt, _, _, [winner]} = at(game, 1)
 
-    game
-    |> find(fn {_, _, b} -> any?(b, &wins?/1) end)
-    |> then(fn {x, _, bs} -> bs |> find(&wins?/1) |> score() |> then(&(&1 * x)) end)
-    |> IO.inspect(label: "part 1")
+winner
+|> reduce(&MapSet.union/2)
+|> sum()
+|> then(&(&1 * nxt))
+|> IO.inspect(label: "part 1")
 
-    [running, finished] = Stream.chunk_by(game, fn {_, _, b} -> all?(b, &wins?/1) end) |> take(2)
+{nxt, _, _, [loser]} = find(game, &match?({_, _, [], _}, &1))
 
-    loser_index =
-      running
-      |> to_list()
-      |> List.last()
-      |> elem(2)
-      |> find_index(fn bd -> !wins?(bd) end)
-
-    {nxt, _, boards} = at(finished, 0)
-
-    boards
-    |> at(loser_index)
-    |> score()
-    |> then(&(&1 * nxt))
-    |> IO.inspect(label: "part 2")
-  end
-end
-
-B.run()
+loser
+|> reduce(&MapSet.union/2)
+|> sum()
+|> then(&(&1 * nxt))
+|> IO.inspect(label: "part 2")
