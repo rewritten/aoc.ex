e534a3e0733b090c15f4f7470979502a7422604e
Saverio Trioni <saverio.trioni@gmail.com>
2021-12-06 08:34:07 +0000

    6

---
+++ 2021/6.exs
@@ -0,0 +1,25 @@
+#! /usr/bin/env elixir
+
+data =
+  "input/2021/6.txt"
+  |> File.read!()
+  |> String.trim()
+  |> String.to_charlist()
+  |> Enum.frequencies()
+  |> Map.delete(?,)
+
+gens = for c <- ?0..?8, do: Map.get(data, c, 0)
+
+1..80
+|> Enum.reduce(gens, fn _, [z | rest] ->
+  update_in(rest ++ [z], [Access.at(6)], &(&1 + z))
+end)
+|> Enum.sum()
+|> IO.inspect(label: "part 1")
+
+1..256
+|> Enum.reduce(gens, fn _, [z | rest] ->
+  update_in(rest ++ [z], [Access.at(6)], &(&1 + z))
+end)
+|> Enum.sum()
+|> IO.inspect(label: "part 2")
