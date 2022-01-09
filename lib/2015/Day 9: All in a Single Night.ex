f86e0570d3289ddc92fd6077b701e19c99afc0b5
Saverio Trioni <saverio.trioni@gmail.com>
2022-01-09 20:15:18 +0100

    Small refactoring

--- lib/2015/Day 9: All in a Single Night.ex
+++ lib/2015/Day 9: All in a Single Night.ex
@@ -1,5 +1,8 @@
 defmodule Aoc.AllInASingleNight do
-  def solve(1, text) do
+  def solve(1, text), do: text |> path_lengths() |> Enum.min()
+  def solve(2, text), do: text |> path_lengths() |> Enum.max()
+
+  defp path_lengths(text) do
     distances =
       text
       |> Input.l(map: :i?, map: fn [from, _, to, dist] -> {MapSet.new([from, to]), dist} end)
@@ -10,29 +13,11 @@ defmodule Aoc.AllInASingleNight do
     |> Enum.reduce(&MapSet.union/2)
     |> Enum.to_list()
     |> Perm.permutations()
-    |> Stream.map(fn path -> distance(path, distances) end)
-    |> Enum.min()
-  end
-
-  def solve(2, text) do
-    distances =
-      text
-      |> Input.l(map: :i?, map: fn [from, _, to, dist] -> {MapSet.new([from, to]), dist} end)
-      |> Map.new()
-
-    distances
-    |> Map.keys()
-    |> Enum.reduce(&MapSet.union/2)
-    |> Enum.to_list()
-    |> Perm.permutations()
-    |> Stream.map(fn path -> distance(path, distances) end)
-    |> Enum.max()
-  end
-
-  defp distance(path, distances) do
-    path
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.map(fn pair -> Map.get(distances, MapSet.new(pair)) end)
-    |> Enum.sum()
+    |> Stream.map(fn path ->
+      path
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.map(fn pair -> Map.get(distances, MapSet.new(pair)) end)
+      |> Enum.sum()
+    end)
   end
 end
