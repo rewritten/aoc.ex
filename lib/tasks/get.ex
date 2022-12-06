9f93c40cf917f15faf94a82fefd5f0c0d4b444fc
Saverio Trioni <saverio.trioni@gmail.com>
2022-12-06 09:10:32 +0100

    Reorganization

--- lib/tasks/get.ex
+++ lib/tasks/get.ex
@@ -17,10 +17,8 @@ defmodule Mix.Tasks.Get do
 
     get_input(year, day)
 
-    {text, title, module_name} = get_text(year, day)
-    save_text(year, title, text)
-    create_code(year, title, module_name)
-    create_test(year, day, title, module_name)
+    text = get_text(year, day)
+    save_text(year, text)
   end
 
   def get_input(year, day) do
@@ -37,7 +35,7 @@ defmodule Mix.Tasks.Get do
   def get_text(year, day) do
     problem_html = get!("#{year}/day/#{day}")
 
-    {:ok, problem_markdown} = Pandex.html_to_gfm(problem_html, ~w[--backtick_code_blocks])
+    {:ok, problem_markdown} = Pandex.html_to_gfm(problem_html)
 
     [_, problem_markdown] = String.split(problem_markdown, "<div role=\"main\">")
 
@@ -53,24 +51,12 @@ defmodule Mix.Tasks.Get do
           String.split(problem_markdown, "To begin, ")
       end
 
-    # [problem_markdown, _] = String.split(problem_markdown, "Both parts of this puzzle")
-    problem_markdown = String.trim(problem_markdown)
-    [_, title | _] = String.split(problem_markdown, "---")
-    title = String.trim(title)
-
-    module_name =
-      title
-      |> String.split(":")
-      |> Enum.at(1)
-      |> String.downcase()
-      |> String.replace(~w[' ? ! ,], "")
-      |> String.replace(~r{[ -]}, "_")
-      |> Macro.camelize()
-
-    {problem_markdown, title, module_name}
+    String.trim(problem_markdown)
   end
 
-  def save_text(year, title, text) do
+  def save_text(year, text) do
+    title = text |> String.split("---", parts: 3) |> Enum.at(1) |> String.trim()
+
     text =
       text
       |> String.replace(~r[## --- (Day (\d+)): (.+) ---\n], """
@@ -83,51 +69,19 @@ defmodule Mix.Tasks.Get do
       ## \\3
       """)
       |> String.replace(~r[## --- Part Two ---\n], """
+      ```elixir
+      ```
+
       ## Part Two
       """)
+      |> Kernel.<>("""
 
-    File.write("text/#{year}/#{title}.livemd", text)
-  end
-
-  def create_code(year, title, module_name) do
-    if File.exists?("lib/#{year}/#{title}.ex") do
-      IO.puts("Code file already exists")
-    else
-      IO.puts("Creating code file...")
-
-      File.write!("lib/#{year}/#{title}.ex", """
-      defmodule Aoc.#{module_name} do
-        def solve(1, text) do
-        end
-
-        def solve(2, text) do
-        end
-      end
+      ```elixir
+      ```
       """)
-    end
-  end
 
-  def create_test(year, day, title, module_name) do
-    test_file_contents = File.read!("test/#{year}.exs")
-
-    if String.contains?(test_file_contents, title) do
-      IO.puts("Tests already exist")
-    else
-      IO.puts("Adding tests...")
-
-      new_test = """
-
-        describe "#{title}" do
-          @describetag mod: Aoc.#{module_name}, input: File.read!("input/#{year}/#{day}.txt")
-
-          test! part: 1, expected: 0
-          test! part: 2, expected: 0
-        end
-      """
-
-      new_test_file = Regex.replace(~r"(?=end\n\z)", test_file_contents, new_test)
-      File.write!("test/#{year}.exs", new_test_file)
-    end
+    File.mkdir_p!("#{year}")
+    File.write("#{year}/#{title}.livemd", text)
   end
 
   defp get!(url) do
